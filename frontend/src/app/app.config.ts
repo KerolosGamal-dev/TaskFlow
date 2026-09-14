1a2058769a50339046f16732ca74fb229d2ce94a
David-Gamal <dodo.jamal2021@gmail.com>
2026-09-14 08:31:48 -0900

    Add frontend dashboard and todolist

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations'; 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations()
+]
 };
